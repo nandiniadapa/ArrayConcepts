3dc4ee1d27759e6d0cd498a55ed04dc2f6d1d57d
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-14 13:28:49 +0530

    array intros

--- ArrayIntro.js
+++ ArrayIntro.js
@@ -1,6 +1,20 @@
 document.getElementById("btnArray").addEventListener("click",function(){
     debugger;
     var indexNumber=Number(document.getElementById("IndexNumber").value);
-    var shoppingApps=["myntra","ajio","meesho","flipkart","amazon"];
+    var shoppingApps=["myntra","ajio","meesho","flipkart","amazon",];
     document.getElementById("pResult").innerHTML=shoppingApps[indexNumber];
+});
+document.getElementById("btnFirstElement").addEventListener("click",function(){
+    debugger;
+    var indexNumber=Number(document.getElementById("IndexNumber").value);
+    var shoppingApps=["myntra","ajio","meesho","flipkart","amazon",];
+    document.getElementById("pResult").innerHTML=shoppingApps[0];
+
+});
+document.getElementById("btnLastElement").addEventListener("click",function(){
+    debugger;
+      var indexNumber=Number(document.getElementById("IndexNumber").value);
+    var shoppingApps=["myntra","ajio","meesho","flipkart","amazon",];
+    document.getElementById("pResult").innerHTML=shoppingApps[shoppingApps.length-1];
+
 });
