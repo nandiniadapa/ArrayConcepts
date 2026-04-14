e158ebcb9583a95cabf3b84b58ef8f5ebf2e4f45
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-14 16:53:17 +0530

    array to array

---
+++ AddToArray.js
@@ -0,0 +1,9 @@
+
+var flowers=[];
+document.getElementById("btnAddtoArray").addEventListener("click",function(){
+    debugger;
+    var flower=document.getElementById("txtFlower").value;
+    
+    flowers.push(flower)
+    document.getElementById("pResult").innerHTML=flowers;
+})
