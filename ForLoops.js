8532c5c09f655d0bab6f3aac53042e6d8aaa5b9a
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-30 15:25:21 +0530

    for loops

--- ForLoops.js
+++ ForLoops.js
@@ -1,9 +1,48 @@
-document.getElementById("btnsubmit").addEventListener("click",function(){
+document.getElementById("btncube").addEventListener("click",function(){
     debugger;
     var number=Number(document.getElementById("txtnumber").value);
-    for (i=1;i<=number;i=i++){
+    for (i=1;i<=number;i++){
         debugger;
-        console.log(i)
+        console.log(i*i*i)
     }
                  
 });
+
+document.getElementById("btnsquare").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtnumber").value);
+    for (i=1;i<=number;i++){
+        debugger;
+        console.log(i*i)
+    }
+});
+
+
+document.getElementById("btnNumberPrinter").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtnumber").value);
+    for (i=1;i<=number;i++){
+        debugger;
+        console.log(i)
+    }
+});
+
+document.getElementById("btnEven").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtnumber").value);
+    for (i=2;i<=100;i=i+2){
+        debugger;
+        console.log(i)
+    }
+});
+
+document.getElementById("btnodd").addEventListener("click",function(){
+    debugger;
+    var number=Number(document.getElementById("txtnumber").value);
+    for (i=1;i<=100;i=i+2){
+        debugger;
+        console.log(i)
+    }
+});
+
+
