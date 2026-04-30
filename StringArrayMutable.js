7f3ed78b89117b0cde9f2bec73b34832abe9530e
nandiniadapa <nnandiniadapa@gmail.com>
2026-04-30 11:35:15 +0530

    stringarraymutable.js

--- StringArrayMutable.js
+++ StringArrayMutable.js
@@ -1,10 +1,10 @@
 document.getElementById("btnSubmit").addEventListener("click",function(){
 debugger;
-var Name= [flowers,cars,bikes,ActiveObjects]
-    document.getElementById("pResult").innerHTML = Name;
-    var index = document.getElementById("txtIndex").value;
-    var changeName = document.getElementById("txtChangeName").value;
+    var names = ["nandini","jaanu","hima","navya","viparna"];
+    document.getElementById("pResult").innerHTML = names;
+      var index = document.getElementById("txtIndex").value;
+      var changeName = document.getElementById("txtChangeName").value;
 
-    Name[index] = changeName;
-    document.getElementById("pResultMutable").innerHTML = Name;
+    names[index] = changeName;
+    document.getElementById("pResultMutable").innerHTML = names;
 });
