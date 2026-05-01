bd92cdffe29f3a6a386167f52a51aea661e53385
nandiniadapa <nnandiniadapa@gmail.com>
2026-05-01 12:36:34 +0530

    string arrays

--- StringArray.js
+++ StringArray.js
@@ -3,7 +3,7 @@ document.getElementById("btnStringArray").addEventListener("click", function () 
     var colours = ["Blue", "Red", "yellow"];
     var content = "";
     for (i = 0; i < colours.length; i++) {
-        content = content + "<p> " + (i + 1) + "" + colours[i + 1] + "</p>";
+        content = content + "<p> " + (i + 1) + "." + colours[i ] + "</p>";
         document.getElementById("divResult").innerHTML = content;
     }
 });
